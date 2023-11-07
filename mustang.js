2dbff8161198b6a3d2d24dacf3e384338d4d1336
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2023-11-08 02:22:14 +0900

    Add floor image

--- mustang.js
+++ mustang.js
@@ -7,7 +7,7 @@ window.onload = function init() {
     renderer.setSize(canvas.width, canvas.height);
 
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x000000);
+    scene.background = new THREE.Color(0xffffff); // Set the background color to white (0xffffff)
 
     const camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000);
     camera.rotation.y = 45 / 180 * Math.PI;
@@ -17,21 +17,21 @@ window.onload = function init() {
 
     const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // 조명 색을 백색으로 변경
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1.1); // Light color changed to white
     scene.add(ambientLight);
 
-    const pointLight = new THREE.PointLight(0xffffff, 1); // 조명 색을 백색으로 변경
-    pointLight.position.set(0, 1000, 1000);
-    scene.add(pointLight);
+    // const pointLight = new THREE.PointLight(0xffffff, 0.1); // Light color changed to white
+    // pointLight.position.set(0, 1000, 1000);
+    // scene.add(pointLight);
 
     const loader = new THREE.GLTFLoader();
-    loader.load('./model/AquaRoad.gltf', function (gltf) {
+    loader.load('./model/Maple2.gltf', function (gltf) {
         const car = gltf.scene.children[0];
         car.scale.set(0.5, 0.5, 0.5);
 
         const material = new THREE.MeshStandardMaterial({
-            color: 0xaaaaaa, // 모델의 머터리얼 색상을 변경
-            roughness: 0.75, // 머터리얼 설정 조절 (조명 강도 조절에 도움을 줄 수 있음)
+            color: 0xaaaaaa, // Change the material color
+            roughness: 0.75,
         });
 
         car.material = material;
