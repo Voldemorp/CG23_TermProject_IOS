6a5ae4ea270091ca57d53a7be7bd28fed3dc0c73
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2023-11-08 19:55:23 +0900

    Add audio

--- mustang.js
+++ mustang.js
@@ -19,12 +19,94 @@ window.onload = function init() {
 
     const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1.1 ); // Light color changed to white
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1.1); // Light color changed to white
     scene.add(ambientLight);
 
-    const pointLight = new THREE.PointLight(0xffffff, 0.1); // Light color changed to white
-    pointLight.position.set(0, 1000, 1000);
-    scene.add(pointLight);
+    // const pointLight = new THREE.PointLight(0xffffff, 0.1); // Light color changed to white
+    // pointLight.position.set(0, 1000, 1000);
+    // scene.add(pointLight);
+    
+    //아쿠아로드 노래 재생
+    // const backgroundAudio = document.getElementById("AquaRoad_audio");
+
+    // // 오디오 자동 재생 (선택 사항)
+    // backgroundAudio.autoplay = true;
+
+    // // 노래가 무한 반복되도록 설정 (선택 사항)
+    // backgroundAudio.loop = true;
+
+    // // 오디오 재생
+    // backgroundAudio.play();
+
+    // 오디오 일시 정지
+    // backgroundAudio.pause();
+
+    // Create an AudioContext after a user gesture (e.g., click)
+    // document.addEventListener('click', function() {
+    //     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+    //     // 이제 오디오 로딩 및 재생을 수행할 수 있음
+    //     const audioLoader = new THREE.AudioLoader();
+    //     audioLoader.load('./model/audio/AquaRoad.mp3', function(buffer) {
+    //       const listener = new THREE.AudioListener();
+    //       const sound = new THREE.Audio(listener);
+    //       sound.setBuffer(buffer);
+    //       sound.setLoop(true);
+    //       sound.setVolume(0.5);
+    //       sound.play();
+    //     });
+    //   });
+    // 카메라의 현재 위치를 가져옵니다.
+    // 카메라 위치를 기준으로 어떤 노래를 재생할지 결정합니다.
+    let audioFilePath;
+    if (camera.position.x < 50 && camera.position.y < 50 && camera.position.z < 50) {
+        audioFilePath = './model/audio/AquaRoad.mp3'; // 첫 번째 노래 파일 경로
+    } else {
+        audioFilePath = './model/audio/Elinia.mp3'; // 두 번째 노래 파일 경로
+    }
+    
+    // 사용자 동작 이후에 오디오 컨텍스트 생성
+    document.addEventListener('click', function() {
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    
+        // 오디오 로딩 및 재생
+        const audioLoader = new THREE.AudioLoader();
+        audioLoader.load(audioFilePath, function(buffer) {
+            const listener = new THREE.AudioListener();
+            const sound = new THREE.Audio(listener);
+            sound.setBuffer(buffer);
+            sound.setLoop(true);
+            sound.setVolume(0.5);
+            sound.play();
+        });
+    });
+    
+    window.addEventListener('mousedown', onDocumentMouseDown, false);
+    function onDocumentMouseDown(event) {
+        event.preventDefault();
+        
+        const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+        const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+    
+        // 마우스 클릭 위치를 3D 공간 좌표로 변환
+        const mouse3D = new THREE.Vector3(mouseX, mouseY, 0.5);
+        mouse3D.unproject(camera);
+    
+        // 마우스 클릭 위치의 좌표를 출력
+        console.log('Mouse click in 3D space:', mouse3D);
+    }
+    //----
+    const cameraPosition = camera.position;
+    
+    // 카메라의 x, y, z 좌표 얻기
+    const cameraX = cameraPosition.x;
+    const cameraY = cameraPosition.y;
+    const cameraZ = cameraPosition.z;
+    
+    console.log(`카메라 위치: X: ${cameraX}, Y: ${cameraY}, Z: ${cameraZ}`);
+    
+    
+
 
     const loader = new THREE.GLTFLoader();
     loader.load('./model/scene.gltf', function (gltf) {
@@ -48,6 +130,6 @@ window.onload = function init() {
         renderer.render(scene, camera);
         requestAnimationFrame(animate);
     }
-    
-    
+
+
 }
